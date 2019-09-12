f289ac4e88e43accb59383f32c88395ee4c4a694
cory burch <coryburch1988@gmail.com>
2019-09-12 14:58:41 -0500

    fixed broken url

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,13 @@
-from django.urls import
+from django.urls import path
+
 from . import views
 urlpatterns = [
 
-    path(r'^$', views.index, name="index"),
-    path(r'^(?P<question_id>[0-9]+)$', views.detail, name="detail"),
-    path(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
-    path (r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote")
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
